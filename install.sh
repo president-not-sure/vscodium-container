2b803f901a89f567f6808de94cb9571c171d8771
President Not Sure <144360653+president-not-sure@users.noreply.github.com>
2025-11-21 17:43:46 -0500

    refactor(project): reorganize project directory structure and refactor any associated code
    Squashed commit of the following:
    
    commit fc5018c4b029bafb7af72e358d59632ba58d70c8
    Author: President Not Sure <144360653+president-not-sure@users.noreply.github.com>
    Date:   Fri Nov 21 17:16:16 2025 -0500
    
        refactor: convert sudoers creation dockerfile instructions into config files to be copied in the image
    
    commit 47a8ca4a4e5891c178c6f0586df07fafe91ba3cc
    Author: President Not Sure <144360653+president-not-sure@users.noreply.github.com>
    Date:   Fri Nov 21 17:11:20 2025 -0500
    
        refactor: copy all files from image/ in the container
    
    commit a7ac3c29520c9233b661823218058dc58c49015b
    Author: President Not Sure <144360653+president-not-sure@users.noreply.github.com>
    Date:   Fri Nov 21 17:01:25 2025 -0500
    
        refactor: uninstall.sh now reflects the new directory structure
    
    commit 7748e4f3591445e88b8b48607562479a26679ea1
    Author: President Not Sure <144360653+president-not-sure@users.noreply.github.com>
    Date:   Fri Nov 21 17:00:44 2025 -0500
    
        refactor: install.sh now reflects the new directory structure
    
    commit e34abc7e8f2151ad455e1ff27936d2e1a973756a
    Author: President Not Sure <144360653+president-not-sure@users.noreply.github.com>
    Date:   Fri Nov 21 16:57:44 2025 -0500
    
        refactor: host/ now contains all files that will be copied into the host
    
    commit fdd389e039f7dfbb8e5d6a3d5a0edbead5b356ed
    Author: President Not Sure <144360653+president-not-sure@users.noreply.github.com>
    Date:   Fri Nov 21 16:57:00 2025 -0500
    
        refactor: image/ now contains all files that will be copied into the container image
    
    commit ccbada6a26033a350822c2d0046fbd9fc7c7ef79
    Author: President Not Sure <144360653+president-not-sure@users.noreply.github.com>
    Date:   Fri Nov 21 16:55:56 2025 -0500
    
        refactor: build/ now contains all the orchestration files

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-install -vD -m 0644 -t ~/.local/share/applications share/applications/vscodium.desktop
-install -vD -m 0644 -t ~/.local/share/icons/hicolor/48x48 share/icons/hicolor/48x48/vscodium.png
-install -vD -m 0644 -t ~/.local/share/icons/hicolor/64x64 share/icons/hicolor/64x64/vscodium.png
-install -vD -m 0644 -t ~/.local/share/icons/hicolor/64x64@2 share/icons/hicolor/64x64@2/vscodium.png
-install -vD -m 0644 -t ~/.local/share/icons/hicolor/128x128 share/icons/hicolor/128x128/vscodium.png
-install -vD -m 0644 -t ~/.local/share/icons/hicolor/128x128@2 share/icons/hicolor/128x128@2/vscodium.png
-install -vD -m 0644 -t ~/.local/share/icons/hicolor/scalable/apps share/icons/hicolor/scalable/apps/vscodium.svg
-sudo install -vD -m 0755 -t /usr/local/bin bin/vscodium
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/applications host/usr/local/share/applications/vscodium.desktop
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/48x48 host/usr/local/share/icons/hicolor/48x48/vscodium.png
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/64x64 host/usr/local/share/icons/hicolor/64x64/vscodium.png
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/64x64@2 host/usr/local/share/icons/hicolor/64x64@2/vscodium.png
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/128x128 host/usr/local/share/icons/hicolor/128x128/vscodium.png
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/128x128@2 host/usr/local/share/icons/hicolor/128x128@2/vscodium.png
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/scalable/apps host/usr/local/share/icons/hicolor/scalable/apps/vscodium.svg
+sudo install -vD -o root -g root -m 0755 -t /usr/local/bin host/usr/local/bin/vscodium
