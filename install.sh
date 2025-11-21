773cf1b6ac538cd6ee7600b0a693567b9adf5abc
President Not Sure <144360653+president-not-sure@users.noreply.github.com>
2025-11-21 18:40:52 -0500

    fix: icon-cache now sees the icons

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-sudo install -vD -o root -g root -m 0644 -t /usr/local/share/applications host/usr/local/share/applications/vscodium.desktop
-sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/48x48 host/usr/local/share/icons/hicolor/48x48/vscodium.png
-sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/64x64 host/usr/local/share/icons/hicolor/64x64/vscodium.png
-sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/64x64@2 host/usr/local/share/icons/hicolor/64x64@2/vscodium.png
-sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/128x128 host/usr/local/share/icons/hicolor/128x128/vscodium.png
-sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/128x128@2 host/usr/local/share/icons/hicolor/128x128@2/vscodium.png
-sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/scalable/apps host/usr/local/share/icons/hicolor/scalable/apps/vscodium.svg
 sudo install -vD -o root -g root -m 0755 -t /usr/local/bin host/usr/local/bin/vscodium
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/applications host/usr/local/share/applications/vscodium.desktop
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/48x48/apps host/usr/local/share/icons/hicolor/48x48/apps/vscodium.png
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/64x64/apps host/usr/local/share/icons/hicolor/64x64/apps/vscodium.png
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/64x64@2/apps host/usr/local/share/icons/hicolor/64x64@2/apps/vscodium.png
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/128x128/apps host/usr/local/share/icons/hicolor/128x128/apps/vscodium.png
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/128x128@2/apps host/usr/local/share/icons/hicolor/128x128@2/apps/vscodium.png
+sudo install -vD -o root -g root -m 0644 -t /usr/local/share/icons/hicolor/scalable/apps host/usr/local/share/icons/hicolor/scalable/apps/vscodium.svg
+# bump the timestamp on the top-level icon directory to update icon‑cache
+sudo touch /usr/local/share/icons/hicolor
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 sudo rm -rfv \
+    /usr/local/bin/vscodium \
     /usr/local/share/applications/vscodium.desktop \
-    /usr/local/share/icons/hicolor/48x48/vscodium.png \
-    /usr/local/share/icons/hicolor/64x64/vscodium.png \
-    /usr/local/share/icons/hicolor/64x64@2/vscodium.png \
-    /usr/local/share/icons/hicolor/128x128/vscodium.png \
-    /usr/local/share/icons/hicolor/128x128@2/vscodium.png \
-    /usr/local/share/icons/hicolor/scalable/apps/vscodium.svg \
-    /usr/local/bin/vscodium
+    /usr/local/share/icons/hicolor/48x48/apps/vscodium.png \
+    /usr/local/share/icons/hicolor/64x64/apps/vscodium.png \
+    /usr/local/share/icons/hicolor/64x64@2/apps/vscodium.png \
+    /usr/local/share/icons/hicolor/128x128/apps/vscodium.png \
+    /usr/local/share/icons/hicolor/128x128@2/apps/vscodium.png \
+    /usr/local/share/icons/hicolor/scalable/apps/vscodium.svg
