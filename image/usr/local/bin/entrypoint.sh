cdda05aed00b5175ca88414958faf4162099a065
President Not Sure <144360653+president-not-sure@users.noreply.github.com>
2025-11-23 05:27:52 -0500

    docs: comments

--- image/usr/local/bin/entrypoint.sh
+++ image/usr/local/bin/entrypoint.sh
@@ -3,7 +3,8 @@
 # Exit script on error
 set -e
 
-# Source the profile
+# Source profile and by extension /etc/profile.d/*
 . /etc/profile
 
+# Start VSCodium and pass the entrypoint's arguments to it
 exec /usr/share/codium/codium "$@"
