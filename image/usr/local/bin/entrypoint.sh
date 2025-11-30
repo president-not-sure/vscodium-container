361d058a78ec9ac61efdad457a4a228e93f01712
President Not Sure <144360653+president-not-sure@users.noreply.github.com>
2025-11-29 19:01:40 -0500

    feat: run container in its own private cgroup controlled by systemd with auto-generated vscodium service

--- image/usr/local/bin/entrypoint.sh
+++ image/usr/local/bin/entrypoint.sh
@@ -7,5 +7,22 @@ set -eo pipefail
 # shellcheck disable=SC1091
 . /etc/profile
 
-# Start VSCodium and pass the entrypoint's arguments to it
-exec /usr/share/codium/codium "$@"
+# Auto-generate app service from the environment 
+cat <<EOF > /etc/systemd/system/vscodium.service
+[Service]
+Type=exec
+User=${HOST_USER_UID}
+Group=${HOST_USER_GID}
+
+$(env | while IFS= read -r line; do
+    printf 'Environment="%s"\n' "${line}"
+done)
+
+# Start the app
+ExecStart=/usr/share/codium/codium ${@}
+# poweroff only stops the container since cgroup is private
+ExecStop=sudo systemctl poweroff
+EOF
+
+# Exec systemd with the auto-generated app service as its default service
+exec /sbin/init --unit=vscodium.service
